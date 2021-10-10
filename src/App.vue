ba8d8f2319bc8370f573be3b96443d762dd85ba4
Dejitaru <dejitaru@gmail.com>
2021-10-10 10:27:07 -0500

    un poquito de diseño

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
+       <div class="container">
+        <div class="row">
 
-    <div class="container">
-        <template v-if="!isUserLogged">
-            <input type="email" v-model="user.email">
-            <input type="password" v-model="user.password">
-            <button type="button" @click="login">Login</button>
-        </template>
-        <template v-else>
-            <h2>Bienvenido {{this.email}}</h2>
+            <template v-if="!isUserLogged">
+                <div class="col-md-4 offset-md-4">
+                    <div class="login-form bg-light mt-4 p-4">
+                        <form action="" method="" class="row g-3">
+                            <h4>Welcome</h4>
+                            <div class="col-12">
+                                <label>Username</label>
+                                <input type="text" name="username" class="form-control" placeholder="Email" v-model="user.email">
+                            </div>
+                            <div class="col-12">
+                                <label>Password</label>
+                                <input type="password" name="password" class="form-control" placeholder="Password" v-model="user.password">
+                            </div>
+                            <div class="col-12">
+                                <button type="button" class="btn btn-dark float-end" @click="login">Login</button>
+                            </div>
+                        </form>
+                        <hr class="mt-4">
+                        <div class="col-12">
+                            <p class="text-center mb-0">Have not account yet? <a href="#">Signup</a></p>
+                        </div>
+                    </div>
+                </div>
+            </template>
+            <template v-else>
+                    <h2>Bienvenido {{this.email}}</h2>
 
-        </template>
+            </template>
+        </div>
     </div>
+
   </div>
 </template>
 
